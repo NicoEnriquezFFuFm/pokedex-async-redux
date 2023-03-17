afecf28acdd8ce822bbec9eaa024d30588ee2d6d
Nicolah <nicolah.enriquez@ffufm.com>
2023-03-17 17:02:51 +0800

    feat: added setup for async redux
    - added storeprovider inside main.dart
    - added constants.dart inside utils folder
    - added home_page.dart feature
    - added analyzer inside analysis_options.yaml

---
+++ lib/features/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:pokedexbootcamp/utils/constants.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Text(
+              homePage,
+              style: TextStyle(
+                fontSize: fontSize,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
