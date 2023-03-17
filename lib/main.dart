a78bbda893284f3b2045c6e48718c6f649735604
Nicolah <nicolah.enriquez@ffufm.com>
2023-03-17 16:15:25 +0800

    Added StoreProvider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:async_redux/async_redux.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:pokedexbootcamp/feature/home_page.dart';
+import 'package:pokedexbootcamp/state/app_state.dart';
 
 void main() {
-  runApp(const MyApp());
+  final store = Store<AppState>(
+    initialState: AppState(),
+    actionObservers: kReleaseMode ? null : [ConsoleActionObserver<AppState>()],
+  );
+  runApp(
+    StoreProvider(
+      store: store,
+      child: const HomePage(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
