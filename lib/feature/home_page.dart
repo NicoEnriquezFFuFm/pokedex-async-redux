4f1953c476f665564c7881bed326e9728746b57e
Nicolah <nicolah.enriquez@ffufm.com>
2023-03-17 16:20:48 +0800

    fix: Fixed naming on import regarding constants naming

--- lib/feature/home_page.dart
+++ lib/feature/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pokedexbootcamp/utils/constant.dart';
+import 'package:pokedexbootcamp/utils/constants.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
