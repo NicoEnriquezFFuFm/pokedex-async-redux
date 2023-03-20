8ef52cb867106f8d531692b8c096b6474dd09596
Nicolah <nicolah.enriquez@ffufm.com>
2023-03-20 11:27:30 +0800

    feat: Implemented api services in pokedex - added the handler directory - added api_client.dart - added api_service.dart - added pokemon_api.dart inside handler directory - added model directory

---
+++ lib/api/handler/pokemon_api.dart
@@ -0,0 +1,13 @@
+import 'package:pokedexbootcamp/api/api_client.dart';
+
+class PokemonApi {
+  PokemonApi({
+    required this.apiClient,
+    required this.baseUri,
+  });
+
+  final ApiClient apiClient;
+  final Uri baseUri;
+
+  //TODO: to be continued
+}
