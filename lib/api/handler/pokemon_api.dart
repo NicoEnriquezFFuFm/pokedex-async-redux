ba9e44b51e290bb36ac335022d111a98ac5f36c9
Nicolah <nicolah.enriquez@ffufm.com>
2023-03-21 14:27:31 +0800

    feat: added additional freezed models - added a function that displays specific details about pokemon - removed additonal functions that displays one specific data - modified the variable placement(pokemons) inside main.dart - modified to list for sub models in pokemon_detail.dart - modified to list for sub models in pokemon_moves.dart

--- lib/api/handler/pokemon_api.dart
+++ lib/api/handler/pokemon_api.dart
@@ -1,11 +1,8 @@
 import 'dart:core';
 
+import 'package:dio/dio.dart';
 import 'package:pokedexbootcamp/api/api_client.dart';
-import 'package:pokedexbootcamp/api/model/pokemon.dart';
 import 'package:pokedexbootcamp/api/model/pokemon_detail.dart';
-import 'package:pokedexbootcamp/api/model/pokemon_detail_abilities.dart';
-import 'package:pokedexbootcamp/api/model/pokemon_moves.dart';
-import 'package:pokedexbootcamp/api/model/pokemon_stats.dart';
 
 typedef Json = Map<String, dynamic>;
 
@@ -18,91 +15,13 @@ class PokemonApi {
   final ApiClient apiClient;
   final Uri baseUrl;
 
-  // TODO: to be continued
-
-  Future<List<Pokemon>> getPokemonList({required String offset, required String limit}) async {
-    final queryParam = <String, dynamic>{};
-
-    queryParam['limit'] = limit;
-    queryParam['offset'] = offset;
-
+  Future<PokemonDetail> getPokemonDetails({required String id}) async {
     final uri = baseUrl.replace(
-      queryParameters: queryParam,
-      path: '${baseUrl.path}/pokemon',
-    );
-
-    return await apiClient.dio.getUri(uri).then((response) {
-      return response.data['results'].map<Pokemon>((dynamic data) => Pokemon.fromJson(data as Json)).toList();
-    });
-  }
-
-  Future<List<Abilities>> getPokemonDetailsAbilities(
-      {required String offset, required String limit, required String id}) async {
-    final queryParam = <String, dynamic>{};
-
-    queryParam['limit'] = limit;
-    queryParam['offset'] = offset;
-
-    final uri = baseUrl.replace(
-      queryParameters: queryParam,
       path: '${baseUrl.path}/pokemon/${id}',
     );
 
-    return await apiClient.dio.getUri(uri).then((response) {
-      return response.data['abilities'].map<Abilities>((dynamic data) => Abilities.fromJson(data as Json)).toList();
-    });
-  }
-
-  Future<List<Moves>> getPokemonDetailsMoves(
-      {required String offset, required String limit, required String id}) async {
-    final queryParam = <String, dynamic>{};
-
-    queryParam['limit'] = limit;
-    queryParam['offset'] = offset;
-
-    final uri = baseUrl.replace(
-      queryParameters: queryParam,
-      path: '${baseUrl.path}/pokemon/${id}',
-    );
-
-    return await apiClient.dio.getUri(uri).then((response) {
-      return response.data['move'].map<Moves>((dynamic data) => Moves.fromJson(data as Json)).toList();
-    });
-  }
-
-  Future<List<Stats>> getPokemonDetailsStats(
-      {required String offset, required String limit, required String id}) async {
-    final queryParam = <String, dynamic>{};
-
-    queryParam['limit'] = limit;
-    queryParam['offset'] = offset;
-
-    final uri = baseUrl.replace(
-      queryParameters: queryParam,
-      path: '${baseUrl.path}/pokemon/${id}',
-    );
-
-    return await apiClient.dio.getUri(uri).then((response) {
-      return response.data['stats'].map<Moves>((dynamic data) => Stats.fromJson(data as Json)).toList();
-    });
-  }
-
-  Future<List<PokemonDetail>> getPokemonDetailsAbilitiesWidth(
-      {required String offset, required String limit, required String id}) async {
-    final queryParam = <String, dynamic>{};
-
-    queryParam['limit'] = limit;
-    queryParam['offset'] = offset;
-
-    //https://pokeapi.co/api/v2/pokemon?limit=151&offset=0
-    https: //pokeapi.co/api/v2/pokemon/1/
-    final uri = baseUrl.replace(
-      queryParameters: queryParam,
-      path: '${baseUrl.path}/pokemon/${id}',
-    );
-
-    return await apiClient.dio.getUri(uri).then((response) {
-      return response.data["abilities"].map<PokemonDetail>((dynamic data) => PokemonDetail.fromJson(data as Json));
-    });
+    Response response = await apiClient.dio.getUri(uri);
+    PokemonDetail pokeResponse = PokemonDetail.fromJson(response.data);
+    return pokeResponse;
   }
 }
