0cea77149ad645693ec0ed841004c224a3fa2867
Nicolah <nicolah.enriquez@ffufm.com>
2023-03-21 15:10:55 +0800

    feat: Added additional freezed models and functions - modified pokemons variable placement, placed below storeprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:pokedexbootcamp/api/api_service.dart';
 import 'package:pokedexbootcamp/state/app_state.dart';
 
 import 'features/home_page.dart';
 
-void main() {
+Future<void> main() async {
   final store = Store<AppState>(
     initialState: AppState(),
     actionObservers: kReleaseMode ? null : [ConsoleActionObserver<AppState>()],
@@ -16,4 +17,7 @@ void main() {
       child: const HomePage(),
     ),
   );
+
+  final pokemons = await ApiService().pokemonApi.getPokemonDetails(id: '1');
+  pokemons;
 }
