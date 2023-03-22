743e5cebe5389f31e1d121299f5ef9078a953266
Nicolah <nicolah.enriquez@ffufm.com>
2023-03-22 11:49:27 +0800

    feat: added additional freezed models - (pr feedback) -  - added the getPokemonList function that gets the list of pokemon

--- lib/api/handler/pokemon_api.dart
+++ lib/api/handler/pokemon_api.dart
@@ -2,6 +2,7 @@ import 'dart:core';
 
 import 'package:dio/dio.dart';
 import 'package:pokedexbootcamp/api/api_client.dart';
+import 'package:pokedexbootcamp/api/model/pokemon.dart';
 import 'package:pokedexbootcamp/api/model/pokemon_detail.dart';
 
 typedef Json = Map<String, dynamic>;
@@ -14,6 +15,22 @@ class PokemonApi {
 
   final ApiClient apiClient;
   final Uri baseUrl;
+
+  Future<List<Pokemon>> getPokemonList({required String offset, required String limit}) async {
+    final queryParam = <String, dynamic>{};
+
+    queryParam['limit'] = limit;
+    queryParam['offset'] = offset;
+
+    final uri = baseUrl.replace(
+      queryParameters: queryParam,
+      path: '${baseUrl.path}/pokemon',
+    );
+
+    return await apiClient.dio.getUri(uri).then((response) {
+      return response.data['results'].map<Pokemon>((dynamic data) => Pokemon.fromJson(data as Json)).toList();
+    });
+  }
 
   Future<PokemonDetail> getPokemonDetails({required String name}) async {
     final uri = baseUrl.replace(
